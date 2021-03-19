fe4116fc456c3ff06ddc272d7cbc1854811e8b34
JefersonV <jvelasquezc15@miumg.edu.gt>
2021-03-19 12:44:02 -0600

    productos de cards renderizados

--- js/app.js
+++ js/app.js
@@ -1,14 +1,34 @@
+const items = document.querySelector('#items')
+const templateCard = document.querySelector('#template-card').content
+/* console.log(templateCard) */
+const fragment = document.createDocumentFragment()
+
 document.addEventListener('DOMContentLoaded', (e) => {
 	fetchData()
 })
+
 
 const fetchData = async () => {
 	try {
 		const res = await fetch('../data.json')
 		const data = await res.json()
-		console.log(data)
-
+		pintarCards(data)
 	} catch (error) {
 			console.log(error)
 	}
+}
+
+let pintarCards = (data) => {
+	console.log(data)
+	/* recorremos la data */
+	data.forEach(producto => {
+		templateCard.querySelector('.card-title').textContent = producto.title
+		templateCard.querySelector('.card-text').textContent = producto.precio
+		templateCard.querySelector('.card-img-top').setAttribute('src', producto.thumbnailUrl)
+		templateCard.querySelector('.card-img-top').setAttribute('alt', producto.title)
+		const clone = templateCard.cloneNode(true)
+
+		fragment.appendChild(clone)
+	})
+	items.appendChild(fragment)
 }
